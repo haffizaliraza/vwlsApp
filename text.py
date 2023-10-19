4334b03ce3a36fe9fc0ea15cdfef8953123bcc85
aliraza <haffizaliraza@gmail.com>
2023-10-20 01:51:10 +0500

    You miss 100% of the shots you don't take.

--- text.py
+++ text.py
@@ -1,3 +1,3 @@
 def nonewlines(s: str) -> str:
     return s.replace("\n", " ").replace("\r", " ")
-Life is a journey, not a destination.
+Life is a journey, not a destination.You miss 100% of the shots you don't take.
