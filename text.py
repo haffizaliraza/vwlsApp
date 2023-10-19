caa06af5b552e1bc94d5c58ca58fd2687ff15d2a
aliraza <haffizaliraza@gmail.com>
2023-10-20 02:10:32 +0500

    The only thing necessary for the triumph of evil is for good men to do nothing.

--- text.py
+++ text.py
@@ -1,3 +1,3 @@
 def nonewlines(s: str) -> str:
     return s.replace("\n", " ").replace("\r", " ")
-Life is a journey, not a destination.You miss 100% of the shots you don't take.
+Life is a journey, not a destination.You miss 100% of the shots you don't take.The only thing necessary for the triumph of evil is for good men to do nothing.
